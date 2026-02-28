dbb99017e61d01f6335f2db11c88adaebe45132e
Diego Bello Santalla <diego.bello@vasscompany.com>
2026-02-28 17:40:21 +0100

    initial

--- src/components/PlantDetail.js
+++ src/components/PlantDetail.js
@@ -82,14 +82,7 @@ export default function PlantDetail({ plant, onBack, onEdit, onDelete, onWater, 
     setConfirmDeletePhoto(false);
   };
 
-  // Determine if current swiper photo is deletable and what type
-  const currentSwiperPhoto = swiperPhotos[photoIndex];
-  const currentIsMainPhoto = currentSwiperPhoto?.label === "Planta";
-  const currentMainPhotoIndex = currentIsMainPhoto
-    ? mainPhotos.indexOf(mainPhotos.find(p => p === currentSwiperPhoto?.src))
-    : -1;
-  const currentIsFloweringPhoto = currentSwiperPhoto?.label === "🌸 Floración";
-  const currentIsDeletable = currentIsMainPhoto || currentIsFloweringPhoto;
+
 
   return (
     <div style={{ paddingTop: 24 }} className="fade-in">
